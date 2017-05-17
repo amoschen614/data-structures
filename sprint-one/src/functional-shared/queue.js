ca772163191a76685fef3c3e555d2be57068c385
Hack Reactor Student <student@hackreactor.com>
2017-05-17 15:46:56 -0700

    Add functionality for functional shared | stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.headIndex = 0;
+  instance.tailIndex = 0;
+  instance.storage = {};
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.tailIndex++] = value;
+};
 
+queueMethods.dequeue = function() {
+  if (this.tailIndex - this.headIndex > 0) {
+    const value = this.storage[this.headIndex];
+    delete this.storage[this.headIndex++];
+    return value;
+  }
+}; 
+
+queueMethods.size = function() {
+  return this.tailIndex - this.headIndex;
+};
