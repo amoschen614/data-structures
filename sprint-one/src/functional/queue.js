94d0d342daf0671acbebd5600150fce8881c6934
Hack Reactor Student <student@hackreactor.com>
2017-05-17 14:43:29 -0700

    Add functionality for functional-queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,21 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size = 0;
-  var counter = 0;
+  var tailIndex = 0;
+  var headIndex = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[counter++] = value;
+    storage[tailIndex++] = value;
   };
 
   someInstance.dequeue = function() {
-    
+    if (tailIndex - headIndex > 0) {
+      var headEl = storage[headIndex];
+      delete storage[headIndex++];
+      return headEl;
+    }
   };
 
   someInstance.size = function() {
-    return size;
+    return tailIndex - headIndex;
   };
 
   return someInstance;
