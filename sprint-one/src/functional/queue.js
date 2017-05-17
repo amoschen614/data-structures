c5395cc7e3bfbc139df204d5953bb5ac50e410eb
Hack Reactor Student <student@hackreactor.com>
2017-05-17 14:36:24 -0700

    Add functionality for functional-stack (late commit)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,21 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter++] = value;
   };
 
   someInstance.dequeue = function() {
+    
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size++] = value;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      const tailEl = storage[size - 1];
+      delete storage[size - 1];
+      size--;
+      return tailEl;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
