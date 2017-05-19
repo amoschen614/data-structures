32b722e5f994434b9636decbc0ca7fbcc3047615
Hack Reactor Student <student@hackreactor.com>
2017-05-19 15:11:50 -0700

    Add functionality to Set methods

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // unique strings
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);  
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    let itemIndex = this._storage.indexOf(item);
+    this._storage.splice(itemIndex, 1);
+  }
 };
 
 /*
